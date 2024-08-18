f1dec795e6abbe5861af728f1a8c30cae3a81719
Matheus Germano <silvaa.matheus112@gmail.com>
2024-08-18 14:00:14 -0300

    Adicionando base do projeto e conexao com banco de dados

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,39 @@
+package model.dao.impl;
+
+import java.util.List;
+
+import model.dao.SellerDao;
+
+public class SellerDaoJDBC implements SellerDao{
+
+	@Override
+	public void insert(model.entities.Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(model.entities.Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Seller(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public model.entities.Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<model.entities.Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
